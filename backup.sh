53034528cc0469250ca4d9e8d7bffcc1e195537f
GITClement59 <51699768+GITClement59@users.noreply.github.com>
2019-06-14 10:01:06 +0200

    Add files via upload

---
+++ backup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#permet de connaitre le nombre de seconde depuis la derniere modif ou un truc du genre
+#mais en tout cas c'est une valeur fixe qui varie dans le temps
+t1=`find /backup/ -printf "%Td-%Tm-%Ty \n" | tail -1`
+t2=`find /home/git/projet-1.git/ -printf "%Td-%Tm-%Ty \n" | tail -1`
+
+#convertir le string provenant du format jj-mm-yy en int
+echo $t1
+echo $t2
+# temps1=$(date -d $t1 +%s)
+# temps2=$(date -d $t2 +%s)
+
+#nommer le fichier et déclarer la source et la destination de l'archive
+TIME=`date +%b-%d-%y`                      
+FILENAME=backup-$TIME.tar.gz    
+SRCDIR=/home/git/projet-1.git                  
+DESDIR=/backup  
+
+#Savoir si il y'a une une modif depuis le dernier archivage
+if  [[ $t1 -ge $t2 ]]
+then 
+    #Savoir si le fichier existe dejà : vu qu'il y'a une modif de temps,on rm et on refait
+    if ! [[ -e $DESDIR/$FILENAME ]]
+    then
+        tar -czf $DESDIR/$FILENAME $SRCDIR
+    else
+        rm -rf $DESDIR/$FILENAME
+        tar -czf $DESDIR/$FILENAME $SRCDIR
+    fi
+else
+    exit
+fi
